4e8794831ab53a3851424c44dd5873e8d7f1a9bb
Parker Argyle <parkerargyle02@gmail.com>
2024-05-04 14:22:19 -0600

    list item deletion (with display) functional

--- financial.js
+++ financial.js
@@ -1,6 +1,6 @@
 const trForm = document.getElementById("new-transaction-form");
 const trSubmit = document.getElementById("new-transaction-submit");
-const trList = document.getElementById("transaction-list");
+const trListDisplay = document.getElementById("transaction-list");
 const errorDiv = document.getElementById("error-div");
 const trArray = [];
 
@@ -11,29 +11,78 @@ trSubmit.addEventListener("click", (e) => {
         setTimeout(() => { errorDiv.textContent = "" }, 1200);
     }
     else {
-        trArray.push({
+        newTr = {
             "name" : trForm.name.value,
             "amount" : trForm.amount.value,
             "date" : trForm.date.value,
             "time" : trForm.time.value
-        });
-
-        const parentItem = document.createElement("li");
-        const trName = document.createElement("h5");
-        const trAmount = document.createElement("div");
-        trName.textContent = trForm.name.value;
-        trAmount.textContent = "$" + trForm.amount.value;
-
-        parentItem.setAttribute("tabindex","0");
-        trName.classList.add("tr-name");
-        trAmount.classList.add("tr-amount");
-
-        parentItem.appendChild(trName);
-        parentItem.appendChild(trAmount);
-        trList.appendChild(parentItem);
+        };
+        trArray.push(newTr);
+        displayAllTr(trArray, trListDisplay);
     }
 });
 
+function displayAllTr(trArray){
+    trListDisplay.innerHTML = "";
+    trArray.forEach(tr => {
+        const parentItem = document.createElement("li");
+        const trName = document.createElement("h5");
+        const trAmount = document.createElement("div");
+        trName.textContent = tr.name;
+        trAmount.textContent = "$" + tr.amount;
+
+        trName.classList.add("tr-name");
+        trAmount.classList.add("tr-amount");
+        
+        parentItem.appendChild(trName);
+        parentItem.appendChild(trAmount);
+        addActions(parentItem, tr);
+        trListDisplay.appendChild(parentItem);
+    });
+}
+
+function addActions(parentItem, trObj) {
+    const editButton = document.createElement("button");
+    
+    editButton.classList.add("material-icons");
+    editButton.textContent = "edit";
+    editButton.addEventListener("click", () => {
+        const trIndex = trArray.indexOf(trObj);
+        // display edit menu, with current transaction values
+    });
+    
+    const deleteButton = document.createElement("button");
+    
+    deleteButton.classList.add("material-icons");
+    deleteButton.textContent = "delete";
+
+    deleteButton.addEventListener("click", () => {
+        const trIndex = trArray.indexOf(trObj);
+        if (trIndex > -1) {
+            trArray.splice(trIndex, 1);
+            displayAllTr(trArray);
+        }
+    });
+
+    parentItem.appendChild(editButton);
+    parentItem.appendChild(deleteButton);
+}
+
+// after a user edits a transaction and clicks "save", this saves the new values to the object. 
+function saveTrEdit(trObj){
+    const trIndex = trArray.indexOf(trObj);
+    trArray[trIndex] = {
+        "name" : trEditForm.name.value,
+            "amount" : trEditForm.amount.value,
+            "date" : trEditForm.date.value,
+            "time" : trEditForm.time.value
+    }
+    displayAllTr(trArray);
+}
+
+function exportTrList(){
+    
+}
 
 /* const hasFileRadio = document.getElementById("has-file");
 const fileInput = document.getElementById("file-input");
