25b3cd723862c0d833a9f14eaec5cef085cd8254
20pargyle <parkerargyle02@gmail.com>
2024-11-23 21:58:25 -0700

    the foundation works now; added basic transaction model

--- _server/core/models.py
+++ _server/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Transaction(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    amount = models.FloatField()
+    place = models.TextField()
+    date = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 export function App() {
     const [login, setLogin] = useState(false);
 
     return (
         <>
-            <Header />
-
+            <p>Beans</p>
         </>
     )
 }
